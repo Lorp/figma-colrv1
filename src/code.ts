afe22212bc2bfed91b1ea1c05c4b5ba261132a76
lorp <lorp@lorp.org>
2023-08-08 03:33:11 +0100

    Major update, plugin essentially complete

--- src/code.ts
+++ src/code.ts
@@ -1,15 +1,3 @@
-//import { createRectangles } from "./rectangle-module";
-//import { SamsaFont, SamsaGlyph, SamsaInstance, SamsaBuffer, SAMSAGLOBAL } from "./samsa-core";
-//import { SamsaFont, SamsaGlyph, SamsaInstance, SamsaBuffer, SAMSAGLOBAL } from "samsa-core";
-// const samsa = require ("samsa-core");
-// const SamsaBuffer = samsa.SamsaBuffer;
-// const SamsaFont = samsa.SamsaFont;
-// const SamsaGlyph = samsa.SamsaGlyph;
-// const SamsaInstance = samsa.SamsaInstance;
-// const SAMSAGLOBAL = samsa.SAMSAGLOBAL
-
-const { SamsaFont, SamsaGlyph, SamsaInstance, SamsaBuffer, SAMSAGLOBAL } = require ("samsa-core");
-
 // Code in this file has access to the *figma document* via the figma global object.
 // Browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
@@ -22,60 +10,70 @@ const { SamsaFont, SamsaGlyph, SamsaInstance, SamsaBuffer, SAMSAGLOBAL } = requi
 // Webpack and bundling
 // https://www.figma.com/plugin-docs/libraries-and-bundling/
 
-//let f = new SamsaFont();
 
-// const GLOBAL = {
-//   samsaFont: null,
-// };
 
+const { fontList } = require("./fontlist.js"); // get list of fonts
+const { emojiMetadata } = require("./emoji_15_0_ordering.js"); // get emoji definitions, originally source is https://github.com/googlefonts/emoji-metadata/blob/main/emoji_15_0_ordering.json
+const { SamsaFont, SamsaGlyph, SamsaInstance, SamsaBuffer, SAMSAGLOBAL } = require ("samsa-core");
+
+// brotli (this works!)
+const buffer = require("buffer");
+const brotliDecompressFunction = require("../node_modules/brotli/decompress");
+
+
+// GLOBAL object
 const GLOBAL:any = {
-	figmaNode: null,
-}
+	figmaNodeId: null,
+};
 
-// This shows the HTML page in "ui.html".
-const options = {
-	width: 300,
-	height: 500,
-	title: "COLRv1 fonts",
-}
-figma.showUI(__html__, options);
-const sceneNodes: SceneNode[] = []; // these are used in the "zoom to fit" step // TODO: if the UI is persistent, we need to reconsider how we zoom to fit each time
+// show the HTML page in "ui.html"
+figma.showUI(__uiFiles__.main, { width: 314, height: 600, title: "COLRv1 fonts" });
 
-let font:any= <any>{}; // skip type checking
+
+// this is the main font object: you can only have one font at a time
+let font:any= <any>{};
+
+// get current text color
+const textNodeTest:any = figma.createText();
+const textColor:object = textNodeTest.type == "SOLID" ? textNodeTest.fills[0].color : { r: 0, g: 0, b: 0 };
+textNodeTest.remove();
+
+// set up defaults, send init message to UI
+const defaults = {
+	textColor: textColor,
+};
+
+// initial messages to UI
+figma.ui.postMessage({type: "init", defaults: defaults});
+figma.ui.postMessage({type: "font-list", fontList: fontList});
+figma.ui.postMessage({type: "emoji-ordering", emojiMetadata: emojiMetadata});
+
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = msg => {
 
-
-	// One way of distinguishing between different types of messages sent from
-	// your HTML page is to use an object with a "type" property like this.
-
 	switch (msg.type) {
 
-		case 'create-rectangles': {
-			const nodes: SceneNode[] = [];
-			// for (let i = 0; i < msg.count; i++) {
-			//   const rect = figma.createRectangle();
-			//   rect.x = i * 150;
-			//   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-			//   figma.currentPage.appendChild(rect);
-			//   nodes.push(rect);
-			// }
-			figma.currentPage.selection = nodes;
-			//figma.viewport.scrollAndZoomIntoView(nodes);
+		case "fetch-emojis": {
 
+			const emojiSVGs: {[key: string]: string} = {}; // we return an object of SVGs indexed by the string that invokes the emoji (the UI already knows about the arrangement of emojis)
+			
+			// we return an object of SVGs indexed by string
+			emojiMetadata[msg.emojiType].emoji.forEach((emojiChar:any) => {
+				const str: string = String.fromCodePoint(...emojiChar.base);
+				const strings:string[] = [str];
+				emojiChar.alternates.forEach((alternate:any) => strings.push(String.fromCodePoint(...alternate))) // get the alternate strings as well
+				strings.forEach(str => emojiSVGs[str] = font.renderText({text: str, fontSize: 24, format: "svg"})); // render all the strings to svg, store the svg strings in the emojiSVGs object
+			});
+
+			// send the emoji svgs back to UI using message type "emoji-svgs"
+			figma.ui.postMessage({type: "emoji-svgs", emojiType: msg.emojiType, svgs: emojiSVGs });
 			break;
 		}
 
 		case "fetch-font": {
-			const temp: number = msg ?? 0;
-			console.log(temp);
-
-			console.log("fetch-font");
-			console.log("here we are");
-			console.log(msg);
 
 			fetch(msg.url)
 			.then(response => {
@@ -83,38 +81,60 @@ figma.ui.onmessage = msg => {
 				return response.arrayBuffer();
 			})
 			.then(arrayBuffer => {
-				console.log("Loaded bytes: ", arrayBuffer.byteLength);
+				console.log(`File loaded (${arrayBuffer.byteLength} bytes)`);
 				const options = {
 					fontFace: msg.url,
 					allGlyphs: true,
 					allTVTs: true,
 				};
 
-				font = new SamsaFont(new SamsaBuffer(arrayBuffer), options);
+				// load the font, decompress it if necessary
+				const fingerprint = new DataView(arrayBuffer, 0, 4).getUint32(0);
+				let fontBuffer;
+				if (fingerprint === SAMSAGLOBAL.fingerprints.WOFF2) { // does the fingerprint indicate WOFF2?
+					console.log("WOFF2 font detected");
+					fontBuffer = new SamsaBuffer(arrayBuffer).decodeWOFF2({ // if woff2, convert to ttf here
+						bufferObject: buffer.Buffer, // Samsa doesn’t know about Buffer, so we pass it in
+						brotliDecompress: brotliDecompressFunction, // Samsa doesn’t know about Brotli, so we pass it in the decompress function
+						ignoreInstructions: true,
+						ignoreChecksums: true,
+					});
+					console.log(`WOFF2 font decompressed to TTF (${fontBuffer.byteLength} bytes)`);
+					// TODO: check it really is a TTF!
+				}
+				else {
+					fontBuffer = new SamsaBuffer(arrayBuffer);
+				}
 
-				console.log("Font loaded");
-				console.log(font);
+				// make a SamsaFont object from the uncompressed ttf
+				// - font is a global variable (TODO: have it as GLOBAL.font?)
+				font = new SamsaFont(fontBuffer, options);
+				console.log("TTF font loaded");
+				
+				// signal to the UI that we’ve got the font, by sending its names, fvar, CPAL
+				// - TODO: send these all as one message?
+				figma.ui.postMessage({type: "font-fetched"});
 
-				if (font.fvar) {
+				// variable font?
+				if (font.fvar && font.fvar.axisCount) {
 					font.fvar.axes.forEach((axis:any) => {
 						axis.name = font.names[axis.axisNameID];
 					});
+					figma.ui.postMessage({type: "fvar", fvar: { instances: font.fvar.instances, axes: font.fvar.axes}});	
 				}
 
-				if (font.CPAL) {
+				// color font?
+				if (font.CPAL && (!msg.excludes || !msg.excludes.includes("CPAL"))) { // we might not need CPAL at the front end
 					font.CPAL.hexColors = {}; // must use an object here... (sparse arrays in JSON are HUGE!)
 					font.CPAL.palettes.forEach((palette:any) => {
 						palette.colors.forEach((color:number) => {
 							font.CPAL.hexColors[color] = font.hexColorFromU32(color);
 						});
 					});
+					figma.ui.postMessage({type: "CPAL", CPAL: { colors: font.CPAL.colors, palettes: font.CPAL.palettes, hexColors: font.CPAL.hexColors}});
 				}
 
-				// signal to the UI that we’ve got the font, by sending its names, fvar, CPAL
-				figma.ui.postMessage({type: "names", names: font.names}); // avoid sparseness
-				figma.ui.postMessage({type: "fvar", fvar: font.fvar});
-				figma.ui.postMessage({type: "CPAL", CPAL: font.CPAL});
-
+				figma.ui.postMessage({type: "font-data-delivered"});
 			});
 			break;
 		}
@@ -122,78 +142,47 @@ figma.ui.onmessage = msg => {
 		case "render": {
 
 			// get svg from text, font, size, axisSettings, palette
-			const tuple = font.tupleFromFvs(msg.options.fvs);
+			const tuple = font.tupleFromFvs(msg.options.fvs); // degenerate case is correctly []
 			const instance:typeof SamsaInstance = new SamsaInstance(font, tuple);
-			if (!msg.options.text)
-				msg.options.text = "hello";
-			const layout:any = instance.glyphLayoutFromString(msg.options.text);
-			const fontSize:number = msg.options.fontSize;
-			const upem:number = font.head.unitsPerEm;
-			const colorU32:number = 0x000000ff;
-			const context:any = {
-				font: font,
+			let svgString = font.renderText({
+				text: msg.options.text,
 				instance: instance,
-				paths: {},
-				gradients: {},
-				color: colorU32,
+				fontSize: msg.options.fontSize,
 				paletteId: msg.options.paletteId ?? 0,
-			};
-
-			// set the text as SVG
-			let innerSVGComposition = "";
-			layout.forEach((layoutItem:any) => {
-				const glyph = font.glyphs[layoutItem.id];
-				const iglyph = glyph.instantiate(instance);
-				let thisSVG = iglyph.svg(context); // gets the best possible COLR glyph, with monochrome fallback
-				thisSVG = `<g transform="translate(${layoutItem.ax} 0)" fill="${font.hexColorFromU32(colorU32)}">` + thisSVG + "</g>";
-				innerSVGComposition += thisSVG;
+				format: "svg",
 			});
-			const defs = Object.values(context.paths).join("") + Object.values(context.gradients).join("");
-			const svgPreamble = `<svg xmlns="http://www.w3.org/2000/svg" width="2000" height="2000" viewBox="0 0 2000 2000">`;
-			const svgPostamble = `</svg>`;
-			const scale = fontSize/upem;
-			const gPreamble = `<g transform="scale(${scale} ${-scale}) translate(0 ${-upem})">`;
-			const gPostamble = `</g>`;
 
-			// this is where it comes together
-			const svgString = svgPreamble + (defs ? `<defs>${defs}</defs>` : "") + gPreamble + innerSVGComposition + gPostamble + svgPostamble; // build and insert final SVG
+			// svgString = svgString.replace(/<svg[^>]*>/, "<svg>");
+			// svgString = svgString.replace(/<svg[^>]*>/, "");
+			// svgString = svgString.replace(/<\/svg[^>]*>/, "");
 
+			let node:any = null; // tried node:BaseNode|null = null but that didn’t work, thanks TypeScript!
+			let relativeTransform:Transform|null = null;
+			if (GLOBAL.figmaNodeId !== null) {
+				node = figma.getNodeById(GLOBAL.figmaNodeId);
+				if (node !== null) {
+					relativeTransform = node.relativeTransform;
+					node.remove();
+				}
+				else {
+					GLOBAL.figmaNodeId = null;
+				}
+			}
+			node = figma.createNodeFromSvg(svgString); // convert SVG to node and add it to the page
+			if (relativeTransform) {
+				node.relativeTransform = relativeTransform;
+			}
 
-			if (GLOBAL.figmaNode)
-				GLOBAL.figmaNode.remove();
+			// store node id so we can remove it later
+			GLOBAL.figmaNodeId = node.id;
 
-			let node = figma.createNodeFromSvg(svgString); // convert SVG to node and add it to the page
-			GLOBAL.figmaNode = node;
-
-			//node.setPluginData("samsa-render", msg); // this seems only to take string data
-			sceneNodes.push(node);
-			//figma.currentPage.selection = sceneNodes;
-			//figma.viewport.scrollAndZoomIntoView(sceneNodes);
-
-
-
-
-			/*
-			// add the node to the canvas
-			let node = figma.createNodeFromSvg(svgString); // convert SVG to node and add it to the page
-
-			// LATER, we can try this
-			// set metadata on the node
-			// also set the plugin that made it
-			// also set the date
+			// set node metadata
+			node.setPluginData("creator", "Figma-COLRv1-plugin-LORP");
+			node.setPluginData("dateCreated", new Date().toISOString());
 			node.setPluginData("font", font.names[6]); // PostScript name
-			node.setPluginData("fontSize", msg.fontSize.toString());
-			node.setPluginData("text", msg.text);
-			node.setPluginData("fontVariationSettings", "");
-
-			// zoom to fit
-			sceneNodes.push(node);
-			figma.currentPage.selection = sceneNodes;
-			figma.viewport.scrollAndZoomIntoView(sceneNodes);
-
-			// TEST: add a new element to the UI by sending a message
-			figma.ui.postMessage({type: "AddAxis", tag: "wght"});
-			*/
+			node.setPluginData("fontSize", msg.options.fontSize.toString());
+			node.setPluginData("text", msg.options.text);
+			node.setPluginData("tuple", `[${instance.tuple.join()}]`);
 
 			break;
 		}
@@ -205,7 +194,6 @@ figma.ui.onmessage = msg => {
 				const palette:any = font.CPAL.palettes[paletteId];
 				if (palette && palette.colors[entryId] !== undefined) {
 					palette.colors[entryId] = font.u32FromHexColor(msg.color);
-					//console.log(`updated palette ${paletteId} entry ${entryId} to ${msg.color} as ${color}`);
 				}
 			}
 			break;
@@ -218,5 +206,4 @@ figma.ui.onmessage = msg => {
 	}
 
 };
-
 
---
+++ src/fontlist.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const fontList = {
+	"text": {
+		"Bitter": "https://raw.githubusercontent.com/google/fonts/main/ofl/bitter/Bitter%5Bwght%5D.ttf",
+		"Bitter-Italic": "https://raw.githubusercontent.com/google/fonts/main/ofl/bitter/Bitter-Italic%5Bwght%5D.ttf",
+		"Blaka Ink": "https://raw.githubusercontent.com/google/fonts/main/ofl/blakaink/BlakaInk-Regular.ttf",
+		"Bungee Color": "https://raw.githubusercontent.com/google/fonts/main/ofl/bungeecolor/BungeeColor-Regular.ttf",
+		"Bungee Spice": "https://raw.githubusercontent.com/google/fonts/main/ofl/bungeespice/BungeeSpice-Regular.ttf",
+		"Foldit": "https://raw.githubusercontent.com/google/fonts/main/ofl/foldit/Foldit%5Bwght%5D.ttf",
+		"Gimme Constructo": "https://www.axis-praxis.org/samsa/fonts/figma/GimmeConstructoVariablev03VF.woff2",
+		"Nabla": "https://raw.githubusercontent.com/google/fonts/main/ofl/nabla/Nabla%5BEDPT%2CEHLT%5D.ttf",
+		"Rocher": "https://www.axis-praxis.org/samsa/fonts/figma/RocherColorGX.woff2",
+		"Snoese Color": "https://www.axis-praxis.org/samsa/fonts/figma/SnoeseColorVariable.woff2",
+	},
+	"emoji": {
+		"Noto Color Emoji": "https://www.axis-praxis.org/samsa/fonts/figma/NotoColorEmoji-COLRv1.woff2",
+		"Noto Mono Emoji": "https://www.axis-praxis.org/samsa/fonts/figma/NotoEmoji-VariableFont_wght.woff2",
+	},
+}
+
+export {fontList};
